c7ae1f9bb1e76f538b50846c4fec3eb1092cc21b
An_ran <15804292449@163.com>
2025-04-27 11:46:25 +0800

    init

---
+++ src/main/java/com/anran/highthumb/HighThumbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anran.highthumb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighThumbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighThumbBackendApplication.class, args);
+    }
+
+}
