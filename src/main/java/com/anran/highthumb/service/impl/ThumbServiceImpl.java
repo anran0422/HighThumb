d62093f1f005a2029fbc1ff2b07c1a191d827092
An_ran <15804292449@163.com>
2025-05-17 21:41:41 +0800

    6性能优化-消息队列异步化

--- src/main/java/com/anran/highthumb/service/impl/ThumbServiceImpl.java
+++ src/main/java/com/anran/highthumb/service/impl/ThumbServiceImpl.java
@@ -20,13 +20,9 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
 
-/**
-* @author macbook
-* @description 针对表【thumb】的数据库操作Service实现
-* @createDate 2025-04-27 12:29:00
-*/
 @Slf4j
-@Service("thumbService")
+//@Service("thumbService")
+@Service("thumbServiceLocalCache")
 public class ThumbServiceImpl extends ServiceImpl<ThumbMapper, Thumb>
     implements ThumbService{
 
